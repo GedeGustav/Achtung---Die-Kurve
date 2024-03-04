90f433b31f9a5bb38dde0e2d6a255cbb1e380735
GedeGustav <gustavsvare@gmail.com>
2024-03-04 17:30:07 +0100

    Update Achtung - Die Kurve.py

--- Achtung - Die Kurve.py
+++ Achtung - Die Kurve.py
@@ -45,25 +45,24 @@ class Powerup:
                     pygame.display.flip()
                     powerups.remove(self)
                     
-player_number = 0
 
 class Player:
-    def __init__(self, color, left, right):
-        self.number = player_number
+    def __init__(self, color, left, right, x, y):
         self.color = color
-        self.x = WIDTH / 2
-        self.y = HEIGHT / 2
-        self.speed = 2
-        self.direction = pygame.Vector2(0, 1)
+        self.x = x
+        self.y = y
+        self.direction = (pygame.Vector2(WIDTH/2, HEIGHT/2) - pygame.Vector2(x, y)).normalize()
         self.rect = pygame.Rect(self.x, self.y, 1, 1)
         self.rect.center = (self.x, self.y)
         self.rects = []
         self.invisible_time = 15
         self.mode = power_modes[0]
+        self.power_time = 300
         self.left = left
         self.right = right
 
     def update(self):
+        print(self.direction)
         if self.mode == power_modes[0]:
             self.power_0()
 
@@ -77,7 +76,7 @@ class Player:
             self.power_3()
 
         if self.mode == power_modes[4]:
-            self.power_0()
+            self.power_4()
 
         self.movement()
         self.rect = pygame.Rect(self.x, self.y, 5, 5)
@@ -96,6 +95,7 @@ class Player:
         self.powerup_collision()
 
     def power_0(self):
+        self.power_time = 300
         self.speed = 2
         self.rotation = 4
         keys = pygame.key.get_pressed()
@@ -112,6 +112,10 @@ class Player:
             self.direction.rotate_ip(-self.rotation)
         if keys[self.right]:
             self.direction.rotate_ip(self.rotation)
+        self.power_time -= 1
+        if self.power_time <= 0:
+            self.mode = power_modes[0]
+            self.power_time = 300
 
     def power_2(self):
         self.speed = 1
@@ -121,6 +125,10 @@ class Player:
             self.direction.rotate_ip(-self.rotation)
         if keys[self.right]:
             self.direction.rotate_ip(self.rotation)
+        self.power_time -= 1
+        if self.power_time <= 0:
+            self.mode = power_modes[0]
+            self.power_time = 300
 
     def power_3(self):
         self.speed = 2
@@ -132,9 +140,20 @@ class Player:
             self.direction.rotate_ip(-self.rotation)
         if keys[self.right]:
             self.direction.rotate_ip(self.rotation)
+        self.power_time -= 1
+        if self.power_time <= 0:
+            self.mode = power_modes[0]
+            self.power_time = 300
 
     def power_4(self):
-        pass
+        self.speed = 2
+        self.rotation = 4
+
+        self.power_time -= 1
+        if self.power_time <= 0:
+            self.mode = power_modes[0]
+            self.power_time = 300
+
 
     def movement(self):
         self.x += self.direction.x * self.speed
@@ -148,6 +167,12 @@ class Player:
             if self.rect.colliderect(rect):
                 pygame.quit()
                 exit()
+        for player in players:
+            if player != self:
+                for rect in player.rects:
+                    if self.rect.colliderect(rect):
+                        pygame.quit()
+                        exit()
 
     def powerup_collision(self):
         for powerup in powerups:
@@ -155,12 +180,13 @@ class Player:
                 self.mode = powerup.mode
 
 players = [
-            Player((255, 0, 0), pygame.K_a, pygame.K_d),
-            #Player((0, 255, 0), pygame.K_LEFT, pygame.K_RIGHT),
-            #Player((0, 0, 255), pygame.K_o, pygame.K_p)
+            Player((255, 0, 0), pygame.K_a, pygame.K_d, WIDTH/10, HEIGHT/10),
+            Player((0, 255, 0), pygame.K_LEFT, pygame.K_RIGHT, WIDTH - WIDTH/10, HEIGHT/10),
+            Player((0, 0, 255), pygame.K_o, pygame.K_p, WIDTH/10, HEIGHT - HEIGHT/10),
+            Player((255, 255, 0), pygame.K_v, pygame.K_b, WIDTH - WIDTH/10, HEIGHT - HEIGHT/10)
           ]
 
-powerups = [Powerup(100, 100)]
+powerups = [Powerup(random.randint(100, WIDTH - 100), random.randint(100, HEIGHT - 100)) for i in range(10)]
 
 while True:
     clock = pygame.time.Clock()
@@ -169,14 +195,18 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        """if event.type == pygame.KEYDOWN:
+        if event.type == pygame.KEYDOWN:
             for player in players:
                 if player.mode == power_modes[4]:
                     player.rotation = 90
-                    if event.key == pygame.K_a:
+                    if event.key == player.left:
                         player.direction.rotate_ip(-player.rotation)
-                    if event.key == pygame.K_d:
-                        player.direction.rotate_ip(player.rotation)"""
+                    if event.key == player.right:
+                        player.direction.rotate_ip(player.rotation)
+    
+    
+    
+    
     for player in players:       
         player.update()
         print(player.mode)
